92280988d5927ddf77920db3108ddfe4dbedb24e
jishnusen <jishnu1@gmail.com>
2021-04-11 21:43:01 -0700

    Update spicetify path and fix spotify script

--- .config/polybar/spotify.sh
+++ .config/polybar/spotify.sh
@@ -11,34 +11,9 @@ main() {
   shuffle=$(playerctl shuffle)
   loop=$(playerctl loop)
 
-  BUTTON_COLOR="$(xgetres color5)"
-  ICON_COLOR="$(xgetres color13)"
-  ARTIST_COLOR="$(xgetres color14)"
-
-  pp=
-  if [ "${status}" = "Playing" ]; then
-    pp="%{F${BUTTON_COLOR}}%{F-}"
-  fi
-
-  # ss="%{A1:spotify-refresh $$ playerctl shuffle Off:}%{A}"
-  # if [ "${shuffle}" = "Off" ]; then
-  #   ss="%{A1:spotify-refresh $$ playerctl shuffle On:}%{F${BUTTON_COLOR}}%{F-}%{A}"
-  # fi
-
-  # ll="%{A1:spotify-refresh $$ playerctl loop None:}%{A}"
-  # if [ "${loop}" = "None" ]; then
-  #   ll="%{A1:spotify-refresh $$ playerctl loop Playlist:}%{F${BUTTON_COLOR}}%{F-}%{A}"
-  # fi
-
   # echo -n "%{A1:i3-msg workspace number 9:}%{F${ICON_COLOR}}%{F-}%{A} "
-  echo -n "$artist %{F${BUTTON_COLOR}}-%{F-} "
-  echo -n "%{F${ARTIST_COLOR}}$title%{F-}"
-  # echo -n " %{A1:spotify-refresh $$ playerctl previous:}%{A} "
-  # echo -n "%{A1:spotify-refresh $$ playerctl play-pause:} $pp %{A} "
-  # echo -n "%{A1:spotify-refresh $$ playerctl next:}%{A} "
-  echo -n " "
-  # echo -n "$ss "
-  # echo -n "$ll"
+  echo -n "$artist - "
+  echo -n "$title"
   echo
 }
 
