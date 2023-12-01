2a27ea8404a536fcaa9f8c8a59d9b2fb70bd5c98
Jishnu Sen <jishnu1@gmail.com>
2023-12-01 14:09:10 -0800

    Add fundle

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,1 +1,4 @@
 alias dots '/usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME'
+
+set -gx EDITOR nvim
+alias e $EDITOR
---
+++ .config/fish/conf.d/plugins.fish
@@ -0,0 +1,7 @@
+if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+
+fundle plugin 'edc/bass'
+fundle plugin 'tuvistavie/fish-fastdir'
+fundle plugin 'sjl/z-fish'
+
+fundle init
