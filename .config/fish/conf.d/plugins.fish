208736f8c6b229c806025c77f541e5d7c7a58cea
jishnusen <jishnu1@gmail.com>
2023-12-01 14:20:38 -0800

    Add FZF plugin

--- .config/fish/conf.d/plugins.fish
+++ .config/fish/conf.d/plugins.fish
@@ -3,5 +3,6 @@ if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 fundle plugin 'edc/bass'
 fundle plugin 'tuvistavie/fish-fastdir'
 fundle plugin 'sjl/z-fish'
+fundle plugin 'jethrokuan/fzf'
 
 fundle init
