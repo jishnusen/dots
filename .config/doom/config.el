d5acb2d12bc072a29f1cda18e1df097cfb12d274
Jishnu Sen <jishnu1@gmail.com>
2024-01-12 23:43:16 -0800

    yusss

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -5,7 +5,7 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type nil)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -15,7 +15,7 @@
 (map! :g "C-x C-k" #'kill-this-buffer)
 
 ;;; -- Vi --
-; disable recording macros (i'm too dumb for this feature i think)
+;; disable recording macros (i'm too dumb for this feature i think)
 (map! :n "q" nil)
 
 ;;; -- Init Vars ---
@@ -26,11 +26,13 @@
       ispell-personal-dictionary (concat doom-user-dir "misc/ispell_personal")
       yas-triggers-in-field t
       )
-(setq ispell-program-name "hunspell")
-(ispell-check-version) ;; hack, apparently this makes ispell set its vars correctly
+(after! spell-fu
+  (setq ispell-program-name "hunspell")
+  (ispell-check-version) ;; hack, apparently this makes ispell set its vars correctly
+  )
 
 ;;; -- Magit --
-; fool magit into reading bare repos
+;; fool magit into reading bare repos
 (defun my/magit-process-environment (env)
   "Detect and set git -bare repo env vars when in tracked dotfile directories."
   (let* ((default (file-name-as-directory (expand-file-name default-directory)))
@@ -38,9 +40,11 @@
          (work-tree (expand-file-name "~/"))
          (dotfile-dirs
           (-map (apply-partially 'concat work-tree)
-                (-uniq (-keep #'file-name-directory (split-string (shell-command-to-string
-                (format "/usr/bin/git --git-dir=%s --work-tree=%s ls-tree --full-tree --name-only -r HEAD"
-                        git-dir work-tree))))))))
+                (-uniq (-keep #'file-name-directory
+                              (split-string
+                               (shell-command-to-string
+                                (format "/usr/bin/git --git-dir=%s --work-tree=%s ls-tree --full-tree --name-only -r HEAD"
+                                        git-dir work-tree))))))))
     (push work-tree dotfile-dirs)
     (when (member default dotfile-dirs)
       (push (format "GIT_WORK_TREE=%s" work-tree) env)
@@ -59,14 +63,14 @@
 (setq +latex-viewers '(pdf-tools evince zathura okular skim sumatrapdf)
       font-latex-fontify-script nil)
 
-; stop autocomplete when i'm typing english
+;; stop autocomplete when i'm typing english
 (add-hook 'LaTeX-mode-hook
-      (lambda ()
-        (make-local-variable 'line-move-visual)
-        (setq-local company-minimum-prefix-length 5)))
+          (lambda ()
+            (make-local-variable 'line-move-visual)
+            (setq-local company-minimum-prefix-length 5)))
 
-; - auto insert math snips
-; interactive snippet expand for use with AAS
+;; - auto insert math snips
+;; interactive snippet expand for use with AAS
 (defun insnip (str)
   (lambda () (interactive) (yas-expand-snippet str)))
 
@@ -83,5 +87,28 @@
 
 ;;; -- Common Lisp --
 (after! common-lisp
- (setq sly-command-switch-to-existing-lisp 'never)
- )
+  (setq sly-command-switch-to-existing-lisp 'never)
+  )
+
+;;; -- Org --
+(after! org
+  (setq org-highlight-latex-and-related '(native script entities))
+  (add-to-list 'org-src-lang-modes '("latex-macros" . latex))
+
+  (defvar org-babel-default-header-args:latex-macros
+    '((:results . "raw")
+      (:exports . "results")))
+
+  (defun prefix-all-lines (pre body)
+    (with-temp-buffer
+      (insert body)
+      (string-insert-rectangle (point-min) (point-max) pre)
+      (buffer-string)))
+
+  (defun org-babel-execute:latex-macros (body _params)
+    (concat
+     (prefix-all-lines "#+LATEX_HEADER: " body)
+     "\n#+HTML_HEAD_EXTRA: <div style=\"display: none\"> \\(\n"
+     (prefix-all-lines "#+HTML_HEAD_EXTRA: " body)
+     "\n#+HTML_HEAD_EXTRA: \\)</div>\n"))
+  )
--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -1,6 +1,33 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-(package! aas)
+(package! aas :recipe (:host github :repo "ymarco/auto-activating-snippets")
+  :pin "e92b5cffa4e87c221c24f3e72ae33959e1ec2b68")
 (package! laas)
 (package! sly-quicklisp)
+
+(package! org :recipe
+  (:host nil :repo "https://git.tecosaur.net/mirrors/org-mode.git" :remote "mirror" :fork
+         (:host nil :repo "https://git.tecosaur.net/tec/org-mode.git" :branch "dev" :remote "tecosaur")
+         :files
+         (:defaults "etc")
+         :build t :pre-build
+         (with-temp-file "org-version.el"
+           (require 'lisp-mnt)
+           (let
+               ((version
+                 (with-temp-buffer
+                   (insert-file-contents "lisp/org.el")
+                   (lm-header "version")))
+                (git-version
+                 (string-trim
+                  (with-temp-buffer
+                    (call-process "git" nil t nil "rev-parse" "--short" "HEAD")
+                    (buffer-string)))))
+             (insert
+              (format "(defun org-release () \"The release version of Org.\" %S)\n" version)
+              (format "(defun org-git-version () \"The truncate git commit hash of Org mode.\" %S)\n" git-version)
+              "(provide 'org-version)\n"))))
+  :pin nil)
+
+(unpin! org)
