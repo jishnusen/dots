7f4e96a6136caf95e6fbaee6b30ce4f48b8354ed
Jishnu Sen <jishnu1@gmail.com>
2024-03-07 20:42:15 -0800

    simplify dac script

--- .config/polybar/modules/dac_tail.sh
+++ .config/polybar/modules/dac_tail.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-stdbuf -oL pw-top -b | sed -une '/\(^R.*\)\|\(^I.*alsa_output\)/p' \
-                     | sed -ue '/^R.*alsa_output/d' \
-                     | stdbuf -oL tr -s ' ' \
-                     | stdbuf -oL cut -f 10,12,14 -d ' ' \
-                     | stdbuf -oL awk '{ if ($3=="") { print "dead" } else { print $1 " @ " $2 "Hz"}}' \
-                     | stdbuf -oL uniq \
-                     | stdbuf -oL xargs -I{} \
-                     /bin/sh -c '[ "{}" = "dead" ] && echo && exit;
-                                 export card=$(wpctl inspect @DEFAULT_AUDIO_SINK@ | rg "alsa.card =" | rg -o "\d+"); \
-                                 export rate=$(cat "/proc/asound/card$card/pcm0p/sub0/hw_params" | grep rate | cut -f 2 -d " "); \
-                                 printf "$rate Hz (%s)\n" "{}"'
+card=$(wpctl inspect @DEFAULT_AUDIO_SINK@ | rg "alsa.card =" | rg -o "\d+");
+output_rate=$(cat "/proc/asound/card$card/pcm0p/sub0/hw_params" | grep rate | cut -f 2 -d " ");
+
+[ -z "$output_rate" ] && echo && exit
+
+input_rates=$(pw-top -b -n2 | rg "^R.*" \
+              | rg -v "alsa_output" \
+              | tr -s ' ' \
+              | cut -f 10,12,14 -d ' ' \
+              | awk '{ s++; print $1 " @ " $2 "Hz" }' \
+              | tr '\n' ',' \
+              | sed 's/,$/\n/' \
+              )
+echo "$output_rate Hz ($input_rates)"
